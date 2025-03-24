74885d1dd7f6f88dcb18737e1745c5990cac45fd
varunp100288 <varunp100288@gmail.com>
2025-03-24 16:31:39 +0530

    api key

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-// console.log("API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
+ console.log("API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
